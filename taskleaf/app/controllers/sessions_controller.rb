3018595c3e3b21695ef7cec73c1fb37de4a8a273
Masamune Hiroiwa <rider_jump_kick@yahoo.co.jp>
2019-06-05 23:26:40 +0900

    各画面のtypoミスを修正

--- taskleaf/app/controllers/sessions_controller.rb
+++ taskleaf/app/controllers/sessions_controller.rb
@@ -7,11 +7,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: session_params[:email])
 
-    if user&.authenticate(session_params[:password]
+    if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
       redirect_to root_path, notice: 'ログインしました。'
     else
       render :new
+    end
   end
 
   def destroy
@@ -25,5 +26,4 @@ class SessionsController < ApplicationController
   def session_params
     params.require(:session).permit(:email, :password)
   end
-
 end
