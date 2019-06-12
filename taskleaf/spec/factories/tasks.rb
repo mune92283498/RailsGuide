6527f9c26ecc389648e3ab904ccddbe539fd4ea3
Masamune Hiroiwa <rider_jump_kick@yahoo.co.jp>
2019-06-12 23:52:32 +0900

    FactoryBotのインストールと一覧のSystem Specを実装

---
+++ taskleaf/spec/factories/tasks.rb
@@ -0,0 +1,7 @@
+Factorybot.define do
+  factory :task do
+    name { 'テストを書く' }
+    description { ' Rspec & Capybara & FactoryBotを準備する'}
+    user
+  end
+end
---
+++ taskleaf/spec/system/tasks_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'タスク管理機能', type: :system do
+  describe '一覧表示機能' do
+    before do
+      # ユーザーAを作っておく
+      # 作成者がユーザーAであるタスクを作成しておく
+    end
+    context 'ユーザーAがログインしている時' do
+      before do
+        # ユーザーAでログインする
+      end
+
+      it 'ユーザーAが作成した'
+    end
+  end
+end
